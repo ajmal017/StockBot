987a2ae7cb57414ad03cd6d64d149394054a21f5
Valentino Ekaputra <silentrazgriz@gmail.com>
2017-03-29 17:49:13 +0700

    working on stock component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,28 +19,7 @@
 				<div id="chart"></div>
 			</div>
 			<div id="summary">
-				<div class="panel panel-info">
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-sm-3">
-								<div class="stock-code">BUDI</div>
-								<div class="stock-price">98</div>
-							</div>
-							<div class="col-sm-3">
-								<div>RSI</div>
-								<div class="label label-success">OVERSOLD!</div>
-							</div>
-							<div class="col-sm-3">
-								<div>MACD</div>
-								<div class="label label-danger">REBOUND DOWN!</div>
-							</div>
-							<div class="col-sm-3">
-								<div>STOCHASTIC</div>
-								<div class="label label-warning">STABLE</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<stock v-for="stock in stocks" :key="stock[0].stockCode" :stock-code="stock[0].stockCode" :stock-price="stock[0].close"></stock>
 			</div>
 		</div>
 		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -48,6 +27,8 @@
 		<script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
 		<script src="{{ asset('plugins/vue/vue.js') }}"></script>
 		<script src="{{ asset('plugins/vue-resource/vue-resource.js') }}"></script>
+		
+		<script src="{{ asset('js/component/stock.js') }}"></script>
 
 		<script src="{{ asset('js/googleChart.js') }}"></script>
 		<script src="{{ asset('js/stockChart.js') }}"></script>
@@ -58,6 +39,9 @@
 
 			let stockChart = new StockChart('#chart', gChart);
 			stockChart.app.getStockData('WIKA');
+			
+			let stockList = new StockList('#summary', gChart);
+			stockList.app.generateStock();
 		</script>
 	</body>
 </html>
